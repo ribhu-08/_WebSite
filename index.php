883c53b9556962822b270e42cdb2628c06c2270b
rk <ribhukumar08@gmail.com>
2025-07-03 22:30:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Home Page</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+  
+
+  <!-- Scripts -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+  <script href="script/script.js"></script>
+</body>
+
+
+</html>
