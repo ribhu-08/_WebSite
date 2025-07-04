afd4e06d4e214767dd54978aea6b7bdf96d5c27c
ribhu-08 <rihbukumar08@gmail.com>
2025-07-05 02:33:06 +0530

    Home Page al,ost Complete

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container-login");
 const login = document.querySelector(".login-popup");
 const signup = document.querySelector(".signup-popup");
 const btn_login = document.getElementById("login");
 const btn_signup = document.getElementById("signup");
 const body = document.querySelector("body");
-const login_btn = document.querySelector(".facebook");
+const login_btn = document.querySelector(".login-btn");
 const cross = document.querySelectorAll('.login-signup-cross');
+
 
 cross[0].addEventListener('click', () => {
   container.style.visibility = "hidden";
@@ -19,7 +20,6 @@ cross[1].addEventListener('click', () => {
 })
 
 {
-  console.log(cross);
   signup.style.display = "none";
   login.style.display = "none";
 }
@@ -38,4 +38,33 @@ btn_signup.addEventListener('click', () => {
 btn_login.addEventListener('click', () => {
   signup.style.display = "flex";
   login.style.display = "none";
+})
+
+const box = document.querySelector('.categories-sec');
+const l_shift = document.querySelector('.left-slide');
+const r_shift = document.querySelector('.right-slide');
+
+let offset = 0;
+let count = 0;
+let offset_amount = 490;
+
+const l_shift_listner = l_shift.addEventListener('click', () => {
+    if(count <= 0){
+      l_shift.removeEventListener(l_shift_listner);
+      offset = 0;
+    }else{
+      count -= 1;
+      offset += offset_amount;
+      box.style.transform = `translateX(${offset}px)`;
+    }
+
+})
+r_shift.addEventListener('click', () => {
+    if(count <= 4){
+        count++;
+    offset -= offset_amount;
+    box.style.transform = `translateX(${offset}px)`;
+    }
+    
+    
 })
