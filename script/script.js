cc7e655f41b7835588a8e10ca21de72209823bdc
ribhu-08 <rihbukumar08@gmail.com>
2025-07-05 02:32:42 +0530

    Home Pages Completed

--- script/script.js
+++ script/script.js
@@ -5,25 +5,37 @@ const btn_login = document.getElementById("login");
 const btn_signup = document.getElementById("signup");
 const body = document.querySelector("body");
 const login_btn = document.querySelector(".facebook");
+const cross = document.querySelectorAll('.login-signup-cross');
+
+cross[0].addEventListener('click', () => {
+  container.style.visibility = "hidden";
+  signup.style.display = "none";
+  login.style.display = "none";
+})
+cross[1].addEventListener('click', () => {
+  container.style.visibility = "hidden";
+  signup.style.display = "none";
+  login.style.display = "none";
+})
+
 {
+  console.log(cross);
   signup.style.display = "none";
   login.style.display = "none";
 }
 
 login_btn.addEventListener('click', () => {
-  body.style.backgroundColor = "rgba(0,0,0,0.1)";
+  container.style.backgroundColor = "rgba(1, 1, 1, 0.51)";
   container.style.visibility = "visible";
-  login.style.display = "block";
+  login.style.display = "flex";
 })
 
 btn_signup.addEventListener('click', () => {
-  body.style.backgroundColor = "rgba(0,0,0,0.1)";
   signup.style.display = "none";
-  login.style.display = "block";
+  login.style.display = "flex";
 })
 
 btn_login.addEventListener('click', () => {
-  body.style.backgroundColor = "rgba(0,0,0,0.1)";
-  signup.style.display = "block";
+  signup.style.display = "flex";
   login.style.display = "none";
 })
